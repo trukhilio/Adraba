ba74e15477aa2c6c94c8ebed3a4a98183c894977
trukhilio <trukhantech@gmail.com>
2017-10-09 15:25:35 +0300

    comments

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+//find elements that would be sticky
 var containers = document.getElementsByClassName("container");
 
 function sticky(){
@@ -6,11 +7,14 @@ function sticky(){
         nextElement,
         position,
         heightElem,
+        nextPosition,
         positionNextElem;
     for( i = 0; i < containers.length; i++){
         stickyElement = containers[i];
         nextElement = containers[i+1];
-        position = stickyElement.offsetTop;
+        position = stickyElement.offsetTop; //take offset position of the element
+
+        //here we find height of the element
         Array.prototype.map.call(stickyElement.children, function(item){
             if(item.className==="headline"){
                 heightElem = item.clientHeight;
@@ -18,8 +22,13 @@ function sticky(){
             return heightElem
         });
 
-        nextElement !== undefined ? positionNextElem = nextElement.offsetTop : positionNextElem = (stickyElement.nextElementSibling.offsetTop-heightElem);
+        //For last element next element of this pseudo array is undefined but we can find position the next element in the DOM tree
+        nextPosition = stickyElement.nextElementSibling.offsetTop;
 
+        //set next element position
+        nextElement !== undefined ? positionNextElem = nextElement.offsetTop : positionNextElem = nextPosition-heightElem;
+
+        //adding or removing classes depending of the element's position
         if (window.pageYOffset >= position) {
             stickyElement.classList.add("sticky");
         } else {
@@ -32,6 +41,7 @@ function sticky(){
         }
     }
 }
+
 window.onscroll = function() {
     sticky();
 };
